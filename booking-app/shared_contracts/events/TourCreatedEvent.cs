3bfa0666a5b2a387e590e2813c91a162abd283d9
Le Hien <letanhien.it@gmail.com>
2026-04-03 21:48:57 +0700

    refactor: designed folder

---
+++ booking-app/shared_contracts/events/TourCreatedEvent.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shared_contracts.events
+{
+    public class TourCreatedEvent
+    {
+        
+    }
+}
